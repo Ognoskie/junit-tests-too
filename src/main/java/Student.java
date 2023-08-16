286e7fa8e38b165285e3bbc58742c78129582eca
Braden Wittkop <bradenjwittkop@gmail.com>
2023-08-16 15:21:43 -0500

    finished exercise

---
+++ src/main/java/Student.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class Student {
+	private long id;
+	private String name;
+	private ArrayList<Integer> grades;
+
+	Student(long id, String name){
+		this.id = id;
+		this.name  = name;
+		this.grades = new ArrayList<>();
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public ArrayList<Integer> getGrades() {
+		return grades;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setGrades(ArrayList<Integer> grades) {
+		this.grades = grades;
+	}
+}
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,4 +1,65 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import javax.naming.Name;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
 public class StudentTest {
+
+	private Student acctualStudent;
+	private Student emptyStudent;
+	private ArrayList<Integer> emptyArrayList;
+
+
+
+	@Before
+	public void setup() {
+		acctualStudent = new Student(1L, "Steve");
+		emptyArrayList = new ArrayList<>();
+	}
+
+	@Test
+	public void testStudentConstructor() {
+		assertNull(emptyStudent);
+		assertNotNull(acctualStudent);
+
+	}
+
+	@Test
+	public void testStudentGetters() {
+		assertEquals(acctualStudent.getName(), "Steve");
+		assertEquals(acctualStudent.getId(), 1L);
+
+		assertNotNull(acctualStudent.getGrade());
+		assertEquals(acctualStudent.getGrades(), emptyArrayList);
+	}
+
+
+	@Test
+	public void testStudentAddGrade(){
+		acctualStudent.addGrade(50);
+		acctualStudent.addGrade(75);
+		acctualStudent.addGrade(100);
+
+		ArrayList<Integer> expectedGrades = new ArrayList<>(Arrays.asList(50, 75, 100));
+
+		assertEquals(acctualStudent.getGrades(), Arrays.asList(50, 75, 100));
+
+
+	}
+
+	@Test
+	public void testStudentGetGradeAverage() {
+		acctualStudent.addGrade(50);
+		acctualStudent.addGrade(75);
+		acctualStudent.addGrade(100);
+
+		assertEquals(acctualStudent.getGradeAverage(), 75, 0.000001);
+	}
 
 
 }
